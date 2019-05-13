ed6533964a1153cf18039d90ef6a45ea788de909
Xinzhao Li <szxzlee@gmail.com>
2019-05-13 13:00:16 -0400

    Test virtual and override; test GetControlledTank() method

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,10 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
-
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"));
+    GetControlledTank();
+}
 ATank* ATankPlayerController::GetControlledTank() const
 {
-    return Cast<ATank>(GetPawn());
+    ATank* MyTank = Cast<ATank>(GetPawn());
+    UE_LOG(LogTemp, Warning, TEXT("Controlled Tank: %s"), *MyTank->GetName());
+    return MyTank;
 }
 
 
