7aa113e88ea047db69d61b96228170e03f327b14
Xinzhao Li <szxzlee@gmail.com>
2019-05-17 14:57:28 -0400

    Elevate barrel according to the position of crosshair

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,12 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
     // Move the Barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-    UE_LOG(LogTemp, Warning, TEXT("Barrel-Elevate() called at speed %f"), RelativeSpeed);
+    //UE_LOG(LogTemp, Warning, TEXT("Barrel-Elevate() called at speed %f"), RelativeSpeed);
+    RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+    float ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+    float RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+    float Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegree, MaxElevationDegree);
+    //UE_LOG(LogTemp, Warning, TEXT("Barrel-Elevate() called at elevation %f"), Elevation);
+    SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 
