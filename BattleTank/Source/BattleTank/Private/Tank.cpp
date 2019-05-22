423aa322242f10d5022155380e3c0e03c83ced9c
Xinzhao Li <szxzlee@gmail.com>
2019-05-22 15:47:39 -0400

    Bug fixed: TAC nullptr

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -11,14 +11,15 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	UE_LOG(LogTemp, Warning, TEXT("DONKEY: Tank constructs"));
+	//UE_LOG(LogTemp, Warning, TEXT("DONKEY: Tank constructs"));
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("DONKEY: Tank Begin Play"));
+	//UE_LOG(LogTemp, Warning, TEXT("DONKEY: Tank Begin Play"));
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 
--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -13,7 +13,7 @@ UTankAimingComponent::UTankAimingComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-	UE_LOG(LogTemp, Warning, TEXT("DONKEY: TAC constructs"));
+	//UE_LOG(LogTemp, Warning, TEXT("DONKEY: TAC constructs"));
 	// ...
 }
 
